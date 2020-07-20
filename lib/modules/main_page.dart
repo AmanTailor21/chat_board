8a7d2cf6d9209fc364e7faf0e2dac7efd4450202
Aman Tailor <amantailor21@gmail.com>
2020-07-20 10:34:31 +0530

    1.0

--- lib/modules/main_page.dart
+++ lib/modules/main_page.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 
 import 'chat_page.dart';
 
-class MainPage extends StatelessWidget{
+class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
